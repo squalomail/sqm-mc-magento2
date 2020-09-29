c1655c258526f54aa21bd7491f6eaa826cf26268
Đorđe Kesić <dorde.kesic@squalomail.com>
2020-09-29 12:33:58 +0200

    modifies OauthWizard to only show info page and not the actual oauth login page

--- Block/Adminhtml/System/Config/OauthWizard.php
+++ Block/Adminhtml/System/Config/OauthWizard.php
@@ -15,10 +15,7 @@ class OauthWizard extends \Magento\Config\Block\System\Config\Form\Field
 {
     protected $_template    = 'system/config/oauth_wizard.phtml';
 
-    private $_authorizeUri     = "https://login.mailchimp.com/oauth2/authorize";
-    private $_accessTokenUri   = "https://login.mailchimp.com/oauth2/token";
-    private $_redirectUri      = "https://ebizmarts.com/magento/mc-magento2/oauth2/complete.php";
-    private $_clientId         = 390007044048;
+    private $_authorizeUri     = "http://squ.al/plgmagkey";
 
     protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
@@ -36,10 +33,6 @@ class OauthWizard extends \Magento\Config\Block\System\Config\Form\Field
     }
     public function authorizeRequestUrl()
     {
-
-        $url = $this->_authorizeUri;
-        $redirectUri = urlencode($this->_redirectUri);
-
-        return "{$url}?redirect_uri={$redirectUri}&response_type=code&client_id={$this->_clientId}";
+        return $this->_authorizeUri;
     }
 }
